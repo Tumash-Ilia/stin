eb3e6823ae19819139c2dc279934ff4980a3091b
Tumash-Ilia <tumash1ilya@gmail.com>
2023-05-16 00:58:44 +0200

    edit settings

--- bank/views.py
+++ bank/views.py
@@ -4,7 +4,7 @@ from . import models
 
 
 class SimpleTemplateView(TemplateView):
-    template_name = "bank/simple_template.html"
+    template_name = "base.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
