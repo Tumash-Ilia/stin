a494d4506c2963534d3d142560577066eb2b30fc
Tumash-Ilia <tumash1ilya@gmail.com>
2023-05-17 17:34:30 +0200

    create TransactionListView and update payments methods

--- bank/views.py
+++ bank/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView, ListView, FormView
 from djmoney.money import Money
 from . import models
-from .models import Account
+from .models import Account, Transaction
 from .forms import TransactionForm
 from django.shortcuts import get_object_or_404, redirect
 
@@ -40,15 +40,24 @@ class TransactionView(FormView):
         account = get_object_or_404(Account, owner=user, balance_currency=currency)
 
         if method == 'deposit':
+            Transaction.objects.create(user=user, account=account, amount=amount, amount_currency=currency,
+                                       transaction_type='deposit')
             account.balance += amount_money
+            account.save()
         elif method == 'withdraw':
             if account.balance >= amount_money:
+                Transaction.objects.create(user=user, account=account, amount=amount, amount_currency=currency,
+                                           transaction_type='withdraw')
                 account.balance -= amount_money
+                account.save()
             else:
-                # Обработка случая, когда недостаточно средств на счете
-                # Можно выбросить исключение или вернуть сообщение об ошибке
-                pass
-
-        account.save()
+                form.add_error(None, "Insufficient funds for withdrawal.")
+                return self.form_invalid(form)
 
         return redirect(self.success_url)
+
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'bank/transactions_list.html'
+    context_object_name = 'transactions'
