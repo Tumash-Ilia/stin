c5ff8dc7c3fe2fb1018ea43ae16733ddbad3e7b3
Tumash-Ilia <tumash1ilya@gmail.com>
2023-05-17 05:27:31 +0200

    create models

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Account, Transaction
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'balance')
+
+    def currency(self, obj):
+        return obj.balance
+    currency.short_description = 'Currency'
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('account', 'amount', 'timestamp')
--- bank/models.py
+++ bank/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from djmoney.models.fields import MoneyField
+from djmoney.models.validators import MinMoneyValidator
 
 
 # Create your models here.
@@ -8,3 +11,30 @@ class SimpleModel(models.Model):
         verbose_name="Just a field",
         max_length=255
     )
+
+
+class Account(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = MoneyField(max_digits=10, decimal_places=2, null=True, default_currency='CZK',
+                         validators=[MinMoneyValidator(0)])
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = MoneyField(max_digits=10, decimal_places=2, validators=[MinMoneyValidator(0)])
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"
