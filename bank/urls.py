c810569f2ecdc15e03df4af215ed0674f7befb71
Tumash-Ilia <tumash1ilya@gmail.com>
2023-05-17 16:20:26 +0200

    create payment view

--- bank/urls.py
+++ bank/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from bank import views
-from bank.views import AccountListView
+from bank.views import AccountListView, TransactionView
 
 urlpatterns = [
     path('', views.SimpleTemplateView.as_view()),
     path('overview/', AccountListView.as_view(), name='account_list'),
-
+    path('payment/', TransactionView.as_view(), name='payment_view'),
 ]
 
 
