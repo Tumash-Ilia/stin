852b2cf2575eee9c1d4494374605590ebc50cacd
Tumash-Ilia <tumash1ilya@gmail.com>
2023-05-17 18:28:23 +0200

    add convert currencies

--- bank/views.py
+++ bank/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView, FormView
 from djmoney.money import Money
+from djmoney.contrib.exchange.models import convert_money
 from . import models
 from .models import Account, Transaction
 from .forms import TransactionForm
@@ -37,7 +38,15 @@ class TransactionView(FormView):
 
         user = self.request.user
 
-        account = get_object_or_404(Account, owner=user, balance_currency=currency)
+        account = Account.objects.filter(owner=user, balance_currency=currency).first()
+
+        if not account:
+            # Если у пользователя нет счета в выбранной валюте,
+            # выполняем конвертацию валюты в CZK
+            czk_currency = 'CZK'
+            amount_money = convert_money(amount_money, czk_currency)
+            # account = Account.objects.create(user=user, currency=czk_currency, balance=czk_amount)
+            account = get_object_or_404(Account, owner=user, balance_currency=czk_currency)
 
         if method == 'deposit':
             Transaction.objects.create(user=user, account=account, amount=amount, amount_currency=currency,
